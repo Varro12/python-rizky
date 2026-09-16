73ca801e1473b276f9c6c3e1f7da885bce488cb0
rizkyoktavian355-collab <rizky355@gmail.com>
2026-09-16 09:38:48 +0700

    tessss

--- python_latihan.py
+++ python_latihan.py
@@ -6,7 +6,7 @@ jumlah = int(input("Masukkan Jumlah Beli: "))
 total_asli = harga * jumlah
 
 if total_asli >= 100000:
-    diskon = total_asli * 0.1
+    diskon = total_asli * 0.12
     total_bayar = total_asli - diskon
     print("\nSelamat kamu mendapatkan diskon 10%")
 else:
